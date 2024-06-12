7efc858adfb7a7c9e428ee4a75d47d3edb4261c2
manideep <manideep.pagadala@gmail.com>
2024-06-12 18:33:01 +0530

    devolped rest controller and added swagger

--- src/main/java/com/boot/rest/MyRestController.java
+++ src/main/java/com/boot/rest/MyRestController.java
@@ -1,11 +1,22 @@
 package com.boot.rest;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.boot.mysql.DB1Table;
 import com.boot.mysql.Db1Repo;
+import com.boot.oracle.DB2Table;
 import com.boot.oracle.Db2Repo;
 
 @RestController
+@CrossOrigin
 public class MyRestController {
 
 	private Db1Repo db1Repo;
@@ -15,6 +26,43 @@ public class MyRestController {
 		super();
 		this.db1Repo = db1Repo;
 		this.db2Repo = db2Repo;
+	}
+
+	@PostMapping("savetodb1")
+	public ResponseEntity<String> saveToDB1(@RequestBody DB1Table obj) {
+
+		DB1Table save = db1Repo.save(obj);
+		boolean status = save.getId() != null;
+		HttpStatus code = status ? HttpStatus.CREATED : HttpStatus.INTERNAL_SERVER_ERROR;
+		String msg = status ? "Success" : "failed";
+		return new ResponseEntity<>(msg, code);
+	}
+
+	@PostMapping("savetodb2")
+	public ResponseEntity<String> saveToDB2(@RequestBody DB2Table obj) {
+
+		DB2Table save = db2Repo.save(obj);
+		boolean status = save.getId() != null;
+		HttpStatus code = status ? HttpStatus.CREATED : HttpStatus.INTERNAL_SERVER_ERROR;
+		String msg = status ? "Success" : "failed";
+		return new ResponseEntity<>(msg, code);
+	}
+
+	@GetMapping("fetchFromDB1")
+	public ResponseEntity<List<DB1Table>> dataFromDb1() {
+		List<DB1Table> data = db1Repo.findAll();
+		HttpStatus code = !data.isEmpty() ? HttpStatus.OK : HttpStatus.NO_CONTENT;
+		return new ResponseEntity<>(data, code);
+
+	}
+
+	@GetMapping("fetchFromDB2")
+	public ResponseEntity<List<DB2Table>> dataFromDb2() {
+		List<DB2Table> data = db2Repo.findAll();
+		System.out.println(data);
+		HttpStatus code = !data.isEmpty() ? HttpStatus.OK : HttpStatus.NO_CONTENT;
+		return new ResponseEntity<>(data, code);
+
 	}
 
 }
